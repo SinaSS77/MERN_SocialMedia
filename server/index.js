4fc3c8797174557803860f0b1e5b4e1e61e4449f
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-05-27 20:31:59 -0700

    Added User and Friend updates

--- server/index.js
+++ server/index.js
@@ -8,8 +8,9 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import {fileURLToPath} from "url";
-import authRoutes from './routes/auth.js'
-import {register} from './controllers/auth.js'
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import {register} from './controllers/auth.js';
 
 /*Configuration */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,7 +40,8 @@ const upload = multer({ storage });
 app.post("/auth/register", upload.single("picture"), register);
 
 /* Routes */
-app.use("/auth" , authRoutes)
+app.use("/auth" , authRoutes);
+app.use("/users", userRoutes)
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001 ;
